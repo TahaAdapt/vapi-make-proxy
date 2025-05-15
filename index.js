c6406fb1b577210cef0d616f3e7d8aa95eb7d04d
TahaAdapt <taha@adaptmediaagency.com>
2025-05-15 17:39:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
+
 const app = express();
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 const MAKE_WEBHOOK_URL = process.env.MAKE_WEBHOOK_URL;
